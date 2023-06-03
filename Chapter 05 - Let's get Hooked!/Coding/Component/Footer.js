e011a0794e10e8333be12e3f3f24add1f411dbd0
Bhavya Rohilla <bhavyarohilla@pop-os.localdomain>
2023-06-03 18:43:19 +0530

    Fifth-Day

---
+++ Chapter 05 - Let's get Hooked!/Coding/Component/Footer.js
@@ -0,0 +1,17 @@
+// Footer component for footer section
+const Footer = () => {
+    return (
+        <div className="footer">
+            Created by
+            <i className="fa-solid fa-heart"></i>
+            <a href="#"></a>
+            <i className="fa-solid fa-copyright"></i>2023
+            <strong>
+                Food<span>Fire</span>
+            </strong>
+        </div>
+    );
+};
+
+
+export default Footer;
